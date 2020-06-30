86f11c9b9531bd9d03ebaab091d608a8a272b78c
weeblord2030 <57927882+weeblord2030@users.noreply.github.com>
2020-06-30 18:25:27 +0700

    Update geomap.js

--- Chart/Project/geomap.js
+++ Chart/Project/geomap.js
@@ -4,8 +4,8 @@
 
 var svg = d3.select("#my_dataviz")
   .append("svg")
-    .attr("width", 900)
-    .attr("height", 700)
+    .attr("width", 1200)
+    .attr("height", 900)
 	.attr("background-color","black");
 
 
@@ -26,12 +26,20 @@ function cases() {
             "        Total case in the world at the end of May "
         )
         .attr("font-weight", "bold");
+		
+		
+	 var allgroups = ["0-300",
+					"301-3000",
+					"3001-15000",
+					"15001-100000",
+					">100000"]
+   
   
   d3.csv("https://raw.githubusercontent.com/weeblord2030/DS-DV-Project/master/world%20case%2031-05.csv")
 .then(function(data1){
 	d3.json("https://raw.githubusercontent.com/weeblord2030/DS-DV-Project/master/custom.geo%20(1).json")
 	.then(function(data){
-	console.log(data1);
+	
 	
 	for (var i = 0; i < data1.length; i ++){
 	var dataMa = String(data1[i]["Country_code"]);
@@ -45,7 +53,7 @@ function cases() {
 	} 
 	}
 	}
-	console.log(data);
+	
 	
 	var center = d3.geoCentroid(data)
 var scale = 120;
@@ -73,12 +81,52 @@ var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
 	
 
 	
-	
-		var color = d3.scaleLinear()
-	.range(["#00FFFF","yellow"])
-				.domain([
-		d3.min(data1, function(d) { return d["Cumulative_cases"]; }),
-		d3.max(data1, function(d) { return d["Cumulative_cases"]; })])
+		
+		
+		var arr = [];
+		for (i = 0; i < 216; i++){
+		arr.push(parseFloat(data1[i]["Cumulative_cases"]));	
+		
+		}
+		arr.sort(function(a, b) {
+				return a - b;
+			});
+		console.log(arr);
+		
+		
+		
+		 var color1 = d3.scaleOrdinal()
+		.domain(allgroups)
+		.range(["#ffffcc","#a1dab4","#41b6c4","#2c7fb8","#253494"]);
+		
+		 svg.selectAll("myrect")
+      .data(allgroups)
+      .enter()
+      .append("rect")
+        .attr("x", 820)
+        .attr("y", function(d,i){ return 10 + i*(10+5)}) 
+        .attr("width", 10)
+		.attr("height", 10)
+        .style("fill", function(d){ return color1 (d)})
+		
+		
+	 svg.selectAll("mylabels")
+      .data(allgroups)
+      .enter()
+      .append("text")
+        .attr("x", 830 + 10*.8)
+        .attr("y", function(d,i){ return 17 + i*(10+5)})
+        .style("fill", "black")
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+		.style("font-size",14)
+        .style("alignment-baseline", "middle")
+		
+		 var color = d3.scaleQuantile()
+		.domain(arr)
+		.range(["#ffffcc","#a1dab4","#41b6c4","#2c7fb8","#253494"]);
+		
+		
 		
 		 svg.selectAll("path").data(data.features).enter().append("path")
         .attr("d", path)
@@ -96,6 +144,8 @@ var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
 		return "white";}})
 		
 		
+
+		
 	
 	 
 			
@@ -111,7 +161,15 @@ var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
 		
 function death() {
 		svg.selectAll("*").remove();
-	
+		
+		
+	var allgroups = ["<10",
+					"10-20",
+					"20-100",
+					"100-1000",
+					">1000"]
+					
+					
 	svg.append("text") // Add tittle
         .attr("class", "title")
         .attr("text-anchor", "middle")
@@ -129,7 +187,7 @@ function death() {
 .then(function(data1){
 	d3.json("https://raw.githubusercontent.com/weeblord2030/DS-DV-Project/master/custom.geo%20(1).json")
 	.then(function(data){
-	console.log(data1);
+	
 	
 	for (var i = 0; i < data1.length; i ++){
 	var dataMa = String(data1[i]["Country_code"]);
@@ -143,7 +201,7 @@ function death() {
 	} 
 	}
 	}
-	console.log(data);
+	console.log(data1);
 	
 	var center = d3.geoCentroid(data)
 var scale = 120;
@@ -170,7 +228,43 @@ var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
 	
 	
 	
+		var arr = [];
+		for (i = 0; i < 216; i++){
+		arr.push(parseFloat(data1[i]["Cumulative_deaths"]));	
+		
+		}
+		arr.sort(function(a, b) {
+				return a - b;
+			});
+		console.log(arr);
 	
+	
+	 var color1 = d3.scaleOrdinal()
+		.domain(allgroups)
+		.range(["#ffffb2","#fecc5c","#fd8d3c","#f03b20","#bd0026"])
+		
+		 svg.selectAll("myrect")
+      .data(allgroups)
+      .enter()
+      .append("rect")
+        .attr("x", 830)
+        .attr("y", function(d,i){ return 10 + i*(10+5)}) 
+        .attr("width", 10)
+		.attr("height", 10)
+        .style("fill", function(d){ return color1 (d)})
+		
+		
+	 svg.selectAll("mylabels")
+      .data(allgroups)
+      .enter()
+      .append("text")
+        .attr("x", 840 + 10*.8)
+        .attr("y", function(d,i){ return 17 + i*(10+5)})
+        .style("fill", "black")
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+		.style("font-size",14)
+        .style("alignment-baseline", "middle")
 	
 	
 
@@ -179,12 +273,11 @@ var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
 	
 		
 		
-		var color = d3.scaleLinear()
-			.range(["yellow","red"])
-			.domain([
-			d3.min(data1, function(d) { return d["Cumulative_deaths"]; }),
-			d3.max(data1, function(d) { return d["Cumulative_deaths"]; })])
+		var color = d3.scaleQuantile()
+		.domain(arr)
+		.range(["#ffffb2","#fecc5c","#fd8d3c","#f03b20","#bd0026"])
 
+		
 	
 		 svg.selectAll("path").data(data.features).enter().append("path")
         .attr("d", path)
@@ -199,7 +292,7 @@ var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
 		console.log(color(value));
 			} else {
 		//If value is undefined…
-		return "#FFA07A";}})
+		return "#ffffb2";}})
 		
 		
 		
